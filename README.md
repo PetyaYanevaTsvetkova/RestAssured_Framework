f6e14b4a8192f2b507cdffe51c26c056c5de4969
Petya Tsvetkova <petya.yaneva-tsvetkova@estafet.com>
2022-06-16 17:39:40 +0300

    commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Project's Title:
+	RestAssured framework
+
+## Project Description:
+		
+	Theory:
+	What is RestAssured?
+	How we can use RestAssured?
+	Requests structure?
+	How to use auth, headers, body, get, post, put etc. methods
+
+	Practical tasks:
+	By using the open api from the Usseful links:
+	Create Java class with one main method and execute simple RestAssured requests for get.
+	Create different requests that includes headers and body.
+	Read the response and print it to the console.
+
+	Acceptance criteria:
+	As a trainee, 
+	I understand what is RestAssured
+	I know how to structure requests and use the REST methods
+	I know how to read the RestAssured response
+
+## Table of Contents:
+	setupRestAssured
+	README.md file
+	.gitignore file
+	
+
+## How to Install and Run the Project:
+	IDE need to execute the java project
+	 
+## How to Use the Project:
+	Maven need
+
+## Useful links:
+	https://github.com/rest-assured/rest-assured/wiki/Usage
+
+## Add a License
+	no needed
+
+
---
+++ setupRestAssured/src/main/java/myFirstRestAssuredClass.java
@@ -0,0 +1,61 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Header;
+
+public class myFirstRestAssuredClass {
+    final static String URL = "https://www.abv.bg/";
+
+    public static void main(String[] args) {
+        getResponseStatus();
+        getResponseHeaders();
+        getResponseHeader();
+        getResponseBody();
+    }
+
+
+    private static void authentication() {
+        System.out.println(RestAssured.given()
+                .auth()
+                .basic("username", "password")
+                .when().get()
+                .getHeader("Server"));
+    }
+
+    private static void getResponseHeader() {
+        System.out.println(RestAssured
+                .given()
+                .when()
+                .get(URL)
+                .getHeader("Server"));
+    }
+
+    private static void getResponseHeaders() {
+        for (Header header : RestAssured
+                .given()
+                .when()
+                .get(URL)
+                .getHeaders()) {
+            System.out.println(header);
+        }
+    }
+
+    public static void getResponseStatus() {
+        int statusCode = RestAssured
+                .given()
+                .when()
+                .get(URL)
+                .getStatusCode();
+        System.out.println("The response status is " + statusCode);
+        RestAssured.given().when().get(URL).then().assertThat().statusCode(200);
+    }
+
+    public static void getResponseBody() {
+        System.out.println(RestAssured
+                .given()
+                .when()
+                .get(URL)
+                .then()
+                .log()
+                .body()
+                .toString());
+    }
+}
