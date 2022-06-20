dd2f9ab74a0df087ff6083f88b29836445fe0a63
Petya Tsvetkova <petya.yaneva-tsvetkova@estafet.com>
2022-06-20 13:54:40 +0300

    commit

--- setupRestAssured/src/main/java/myFirstRestAssuredClass.java
+++ setupRestAssured/src/main/java/myFirstRestAssuredClass.java
@@ -1,6 +1,10 @@
 import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.Response;
+import org.apache.groovy.util.Maps;
+
+import java.util.List;
+import java.util.Map;
 
 public class myFirstRestAssuredClass {
     final static String URL = "https://api.publicapis.org/";
@@ -8,52 +12,66 @@ public class myFirstRestAssuredClass {
     public static void main(String[] args) {
         RestAssured.baseURI = URL;
 
+
         getResponseStatus();
+
 
         getResponseHeaders();
 
-        getResponseHeader();
+
+        getResponseHeader("Content-Type");
+
 
         Response responseBody = getResponseBodyAll();
         System.out.println(responseBody.body().prettyPrint());
 
-        Response requestWithParams = getRequestWithParams();
+
+        Response requestWithParams = getRequestWithParams(Maps.of(
+                "title", "cat",
+                "description", "Rwanda"));
         requestWithParams.body().prettyPrint();
 
-        Response responseBodyJSON = getResponseBodyJSON();
-        //responseBodyJSON.body().print();
+
+        Response responseBodyJSON = getResponseBodyJSON(Maps.of(
+                "Content-type", "application/json",
+                "Server", "Caddy"));
         responseBodyJSON.body().prettyPrint();
     }
 
-    private static Response getResponseBodyJSON() {
-        return RestAssured
-                .given()
-                .header("Content-type", "application/json")
-                .headers("Server", "Tomcat")
-                .when()
-                .get("categories");
-    }
 
-    private static Response getRequestWithParams() {
+
+    private static <K, V> Response getRequestWithParams(Map map) {
+        System.out.println("Response with parameters: ");
         return RestAssured.given()
                 .with()
-                .queryParam("title", "cat")
-                .queryParam("description", "Rwanda")
-                //.queryParam("auth", "apiKey")
-                //.queryParam("https", "true")
+                .queryParams(map)
                 .when()
                 .get("entries");
     }
 
-    private static void getResponseHeader() {
-        System.out.println(RestAssured
+    private static <K, V> Response getResponseBodyJSON(Map map) {
+        System.out.println("Response with headers: ");
+        return RestAssured
+                .given()
+                .headers(map)
+                .when()
+                .get("categories");
+    }
+
+
+    private static void getResponseHeader(String headerKey) {
+        System.out.println("Header with key " + headerKey + ": ");
+        System.out.print(RestAssured
                 .given()
                 .when()
                 .get(URL)
-                .getHeader("Content-Type"));
+                .getHeader(headerKey));
+        System.out.println();
+        System.out.println();
     }
 
     private static void getResponseHeaders() {
+        System.out.println("Get Response Headers: ");
         for (Header header : RestAssured
                 .given()
                 .when()
@@ -61,6 +79,7 @@ public class myFirstRestAssuredClass {
                 .getHeaders()) {
             System.out.println(header);
         }
+        System.out.println();
     }
 
     public static void getResponseStatus() {
@@ -69,11 +88,13 @@ public class myFirstRestAssuredClass {
                 .when()
                 .get(URL)
                 .getStatusCode();
-        System.out.println("The response status is " + statusCode);
+        System.out.println("The response status is: " + statusCode);
         RestAssured.given().when().get(URL).then().assertThat().statusCode(200);
+        System.out.println();
     }
 
     public static Response getResponseBodyAll() {
+        System.out.println("All records: ");
         return RestAssured
                 .given()
                 .when()
